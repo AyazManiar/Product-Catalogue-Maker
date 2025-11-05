22a4527d4c7b6abfbcac8a99e88d64e963d5c598
AyazManiar <maniarayaz01@gmail.com>
2025-11-05 13:50:35 +0530

    Update productRoute.js

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,8 +1,14 @@
 import express from "express";
+import { getProducts, getProductById, createProduct, updateProduct, deleteProduct } from "../controllers/productController.js";
 
 // /api/products
 const router = express.Router();
 
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 
 export default router;
